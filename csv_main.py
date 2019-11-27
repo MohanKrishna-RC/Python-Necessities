84838c5efe4d87f006d1fca211ac59a8d7d3e7d3
MohanKrishna-RC <mohankrishna.landa@quantiphi.com>
2019-11-27 17:33:46 +0530

    updated

--- csv_main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("all_mus_happy.csv",header=None)
-
-df['positive_labels'] = df[[3,4,5,6,7,8]].apply(lambda x: ', '.join(x[x.notnull()]), axis = 1)
