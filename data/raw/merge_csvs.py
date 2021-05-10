7f46b626ab289c45058e172b67e7c7443282eb3b
MohanKrishna <mohankrishna.landa@bridgei2i.com>
2021-05-10 14:38:07 +0530

    Updated

--- data/raw/merge_csvs.py
+++
@@ -1,9 +0,0 @@
-from glob import glob
-
-with open('Happy/Happy_csv.csv', 'a') as singleFile:
-    for csv in glob('Happy/*.csv'):
-        if csv == 'Happy/Happy_csv.csv':
-            pass
-        else:
-            for line in open(csv, 'r'):
-                singleFile.write(line)
