c63fbac717a9bf7edd48ec20337c16de55f5b535
MohanKrishna <mohankrishna.landa@bridgei2i.com>
2021-09-22 16:51:04 +0530

    Updated

---
+++ data/raw/tweepy_bot.py
@@ -0,0 +1,36 @@
+"""
+four steps common to all Tweepy programs:
+
+1. Import the tweepy package
+2. Set the authentication credentials
+3. Create a new tweepy.API object
+4. Use the api object to call the Twitter API
+"""
+
+"""
+What Is Tweepy?
+Tweepy is an open source Python package that gives you a very convenient way to access the Twitter API with Python. Tweepy includes a set of classes and methods that represent Twitter’s models and API endpoints, and it transparently handles various implementation details, such as:
+
+Data encoding and decoding
+HTTP requests
+Results pagination
+OAuth authentication
+Rate limits
+Streams
+"""
+
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("pGBDoAaEpkliVKBOLwjtcmHGc",
+    "xF3g1wrP50b6BlZEd20u4oVfjgH1FGQcuWUzlQO5aUWOufvlhw")
+auth.set_access_token("622518493-6VcLIPprbQbv9wkcBBPvCle8vsjU9fE85Dq9oStl", 
+    "tH9aKQbQQ1iRdYTcLSsPwitl44BkAc6jilrsU0ifnXvZhq")
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
---
+++ data/raw/web_crawler.py
@@ -0,0 +1,10 @@
+"""
+Write a web crawler that will scrape data from a particular website.
+"""
+#Scrapy is a open source web crawling framework
+
+"""
+Scrapy introduces the idea of a projec with multiple crawlers or spiders in a 
+single project. This concept is useful, if we're writing multiple crawlers of different sections or subdomains
+of a site.
+"""
